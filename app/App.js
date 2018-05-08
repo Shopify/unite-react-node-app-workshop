f849894b4afc1c5f98c2f2ecf8b7ca7d35db2243
Matt <matt.seccafien@shopify.com>
2018-05-08 14:40:33 -0400

    step4

--- app/App.js
+++ app/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import GameList from './components/GameList';
+import Fetch from 'react-fetch-component';
 
 export default function() {
   return (
     <div onMouseOver={() => console.log('Hi')}>
       <h1>Board game loader</h1>
+      <Fetch url="https://boardgameslist.herokuapp.com" as="json">
+        {(fetchResults) => {
+          if (fetchResults.loading) {
+            return <p>Loading</p>
+          }
+
+          if (fetchResults.error) {
+            return <p>fialed to fetch games</p>
+          }
+
+          return <GameList games={fetchResults.data} />
+        }}
+      </Fetch>
     </div>
   );
 }
